e42de249fe984b96c618636bb9cb175d11240230
Danielle Gomides Alkmim <70445660+alkmia@users.noreply.github.com>
2020-12-03 11:21:34 -0300

    identação

--- mineral.js
+++ mineral.js
@@ -8,14 +8,13 @@ function assinar(){
      console.log(select.value); //valor que a pessoa selecioanou aula 08
      const mineral = select.value;
      if(mineral === "Quartzo"){
-         window.location.href = "./páginas/quartzo.html"; //adicionar aqui a página do minearal selecionado
-     } else if (mineral === "Corindon"){
-         window.location.href ="./páginas/corindon.html";
-    } else if (mineral === "Hematita"){
-         window.location.href ="./páginas/hematita.html";
-    } else if (mineral === "Pirita"){
-        window.location.href ="./páginas/pirita.html";
-     } else{
-         alert("selecione uma opção");
-     }
+         window.location.href = "./páginas/quartzo.html"; //adicionar aqui a página do minearal selecionado} 
+         else if (mineral === "Corindon"){
+            window.location.href ="./páginas/corindon.html";} 
+         else if (mineral === "Hematita"){
+            window.location.href ="./páginas/hematita.html";} 
+         else if (mineral === "Pirita"){
+            window.location.href ="./páginas/pirita.html";} 
+         else{
+             alert("selecione uma opção");}
  }
